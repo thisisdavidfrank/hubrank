b42703d0595169977b9a58790ef9f6c4f7c20f3e
David Frank <david.frank@on24.com>
2019-09-07 21:27:26 -0700

    Adding the build folder for ease of deployment by anybody.

---
+++ build/precache-manifest.057d43c16ee223a9e5440b6e83d3ebe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363f25197eaac1da6cbd97fc3f2c6dd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb810936d224558735e0",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "eca9d552bdcc217884e6",
+    "url": "/static/css/main.040c9dc7.chunk.css"
+  },
+  {
+    "revision": "bb810936d224558735e0",
+    "url": "/static/js/2.54d8da27.chunk.js"
+  },
+  {
+    "revision": "eca9d552bdcc217884e6",
+    "url": "/static/js/main.86c7ba06.chunk.js"
+  },
+  {
+    "revision": "9617de1d6db6c1d8c098",
+    "url": "/static/js/runtime~main.48cc6802.js"
+  }
+]);
